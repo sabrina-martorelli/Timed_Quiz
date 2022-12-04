21982e37ebbd937b4b0136f624b8e88963e85905
SLM <sabrina.martorelli@gmail.com>
2022-12-04 18:02:31 +0000

    Amends questions object

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,36 +1,33 @@
 //Array of Javascript fundamentals questions
 var questions = [
     {
-        title: 'Question 1',
-        text: 'Inside which HTML element do we put the JavaScript?',
+        title: 'Inside which HTML element do we put the JavaScript?',
         choices: ['<script>', '<javascript>', '<scripting>', '<js>'],
         answer: '<script>',
     },
 
     {
-        title: 'Question 2',
-        text: 'How do you write "Hello World" in an alert box?',
+        title: 'How do you write "Hello World" in an alert box?',
         choices: ['alertBox("Hello World");','alert("Hello World");','msgBox("Hello World");','msg("Hello World");'],
         answer: 'alert("Hello World");',
     },
 
     {
-        title: 'Question 3',
-        text: 'How to write an IF statement in JavaScript?',
+        title: 'How to write an IF statement in JavaScript?',
         choices: ['if i == 5 then', 'if i = 5 then', 'if i = 5','if (i == 5)'],
         answer: 'if (i == 5)',
     },
 
     {
-        title: 'Question 4',
-        text: 'What is the correct way to write a JavaScript array?',
+    
+        title: 'What is the correct way to write a JavaScript array?',
         choices: ['var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', 'var colors = "red", "green", "blue"', 'var colors = ["red", "green", "blue"]','var colors = (1:"red", 2:"green", 3:"blue")'],
         answer: 'var colors = ["red", "green", "blue"]',
     },
 
     {
-        title: 'Question 5',
-        text: 'Which event occurs when the user clicks on an HTML element?',
+       
+        title: 'Which event occurs when the user clicks on an HTML element?',
         choices: ['onmouseover', 'onchange', 'onclick','onmouseclick'],
         answer: 'onclick',
     },
